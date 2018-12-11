9e9458fad7e1460336b2a645ded4821626f24b30
IntAlgambra <intdelalgambra@gmail.com>
2018-12-11 13:32:28 +0300

    updated server.py

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_sslify import SSLify 
 
 app = Flask(__name__)
+sslify = SSLify(app)
+
+@app_route('/'):
+def index():
+	return('<h1>This is a server app for Dissonance telegram bot</h1>')
 
 if __name__ == '__main__':
 	app.run()
